71224b1310b55f84c8f3b1bbafd87bec1c6b6034
Sebastian Szczecina <sebszczec@wp.pl>
2024-01-12 08:46:36 +0100

    player movement added

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -2,12 +2,22 @@ extends CharacterBody2D
 
 @onready var satelite = $Satelite
 
+@export var PlayerVelocity = 200
 @export var SateliteRotationSpeed = 5
 @export var SateliteRadius = 25
+
 var angle = 0
+var direction = Vector2(0, 0)
 
 func _physics_process(delta):
 	calculateSatelitePosition(delta)
+	
+	direction.x = Input.get_axis("ui_left", "ui_right")
+	direction.y = Input.get_axis("ui_up", "ui_down")
+	
+	velocity = direction.normalized() * PlayerVelocity
+	
+	move_and_slide()
 	
 
 func calculateSatelitePosition(delta):
