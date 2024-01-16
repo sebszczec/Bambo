a0fd95666c925974fd3a6fbd05edd59a3136e5f1
Sebastian Szczecina <sebszczec@wp.pl>
2024-01-16 10:24:33 +0100

    bullets are bouncing now

--- scripts/Bullet.gd
+++ scripts/Bullet.gd
@@ -2,8 +2,22 @@ extends StaticBody2D
 
 @export var Speed = 20
 
+@onready var lifeTimer = $LifeTimer
+
 var velocity = Vector2(0, 0)
+
+
+func _ready():
+	lifeTimer.start()
+
 
 func _physics_process(delta):
 	velocity = velocity.normalized() * Speed
-	move_and_collide(velocity)
+	var collision = move_and_collide(velocity)
+	
+	if collision != null:
+		velocity = velocity.bounce(collision.get_normal())
+
+
+func _on_life_timer_timeout():
+	queue_free()
