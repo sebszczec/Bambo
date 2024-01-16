09a699df12d36f1abdc8bfd2144d73ba4d54fa59
Sebastian Szczecina <sebszczec@wp.pl>
2024-01-16 12:18:59 +0100

    ball enemy is moving now and bouncing

--- scripts/BallEnemy.gd
+++ scripts/BallEnemy.gd
@@ -5,6 +5,10 @@ extends CharacterBody2D
 @onready var collisionShape = $CollisionShape2D
 
 @export var ResizeSpeed = 0.05
+@export_category("Movemement")
+@export var Speed = 2
+@export var Acceleration = 0.1
+
 
 var resizeFactor = 1
 var collisionShapeResizeSpeed = 0
@@ -12,6 +16,9 @@ var collisionShapeResizeSpeed = 0
 func _ready():
 	resizeTimer.start()
 	collisionShapeResizeSpeed = 140 * ResizeSpeed
+	
+	velocity.x = 1
+	velocity.y = 1
 
 
 func _physics_process(delta):
@@ -19,6 +26,12 @@ func _physics_process(delta):
 	sprite.scale.y = sprite.scale.x
 	
 	collisionShape.shape.radius = collisionShape.shape.radius + collisionShapeResizeSpeed * resizeFactor * delta
+	
+	velocity = velocity.move_toward(velocity.normalized() * Speed, Acceleration)
+	var collision = move_and_collide(velocity)
+	
+	if collision != null:
+		velocity = velocity.bounce(collision.get_normal())
 
 
 func _on_resize_timer_timeout():
