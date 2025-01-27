ace908051926564e115b3e3605186935197aca21
Sebastian Szczecina <sebszczec@wp.pl>
2025-01-27 10:02:00 +0100

    refactoring in perks

--- scripts/homing_perk.gd
+++ scripts/homing_perk.gd
@@ -1,12 +1,8 @@
-extends Area2D
+extends Perk
 
 
 func _ready() -> void:
-	var tween = create_tween()
-	var degree = PI / 12
-	tween.set_loops(0)
-	tween.tween_property(self, "rotation", degree, 0.5).set_trans(Tween.TRANS_LINEAR).set_ease(Tween.EASE_OUT)
-	tween.tween_property(self, "rotation", -degree, 0.5).set_trans(Tween.TRANS_LINEAR).set_ease(Tween.EASE_OUT)
+	super._ready()
 
 func _on_area_entered(area: Area2D) -> void:
 	if area.is_in_group("Player"):
---
+++ scripts/perk.gd
@@ -0,0 +1,11 @@
+extends Area2D
+
+class_name Perk
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	var tween = create_tween()
+	var degree = PI / 12
+	tween.set_loops(0)
+	tween.tween_property(self, "rotation", degree, 0.5).set_trans(Tween.TRANS_LINEAR).set_ease(Tween.EASE_OUT)
+	tween.tween_property(self, "rotation", -degree, 0.5).set_trans(Tween.TRANS_LINEAR).set_ease(Tween.EASE_OUT)
