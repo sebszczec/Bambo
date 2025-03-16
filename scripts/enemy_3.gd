a3e1532fe8bce612d98506a42fc1bb7532974260
Sebastian Szczecina <sebszczec@wp.pl>
2025-03-16 09:47:30 +0100

    working on enemy 3

--- scripts/enemy_3.gd
+++ scripts/enemy_3.gd
@@ -1,14 +1,13 @@
 extends CharacterBody2D
 
 @export var Life = 200
-# public
 @export var Damage = 0
-# public
 @export var IsDamageOverTime = false
-# public
 @export var DamageTickTime = 0.0
-# public
 @export var ShootingDelay = 1.0
+@export var MaxSpeed = 10
+@export var Acceleration = 10
+@export var Friction = 5
 
 var floatingTextScene = preload("res://scenes/floating_text.tscn")
 var hitEffectScene = preload("res://scenes/hit_effect.tscn")
@@ -26,18 +25,21 @@ var _deathTimer = Timer.new()
 var _shootingTimer = Timer.new()
 var _isDead = false
 var _weapon = null
+var _world = null
 
 signal killed
 
 func _ready() -> void:
+	_world = get_tree().root
+	
 	var visualSettings = ConfigHandler.load_visuals()
 	_showDamage = visualSettings["show_damage_given"]
 	lifeBar.visible = visualSettings["show_enemies_lifebar"]
 	lifeBar.setColor(Color.GREEN)
 	
 	_weapon = WeaponFactory.get_weapon(Enums.WEAPONS.FIREWORKS)
-	_weapon.set_owner(self)
-	_weapon.register_internal_nodes(self)
+	_weapon.set_owner(_world)
+	_weapon.register_internal_nodes(_world)
 	
 	_deathTimer.one_shot = true
 	_deathTimer.wait_time = 2
@@ -50,8 +52,12 @@ func _ready() -> void:
 	add_child(_shootingTimer)
 	_shootingTimer.start()
 
-func _physics_process(_delta: float) -> void:
-	pass
+func _physics_process(delta: float) -> void:
+	velocity = velocity.move_toward(Vector2(1, 0) * MaxSpeed, Acceleration)
+	if !_isDead:
+		move_and_collide(velocity * delta)
+
+
 
 func get_enemy_name():
 	return "Enemy 3"
@@ -81,7 +87,7 @@ func handleDamage(damage):
 		return false
 
 func _on_shooting_timer_timeout():
-	_weapon.shoot(self, aim.global_position, aim.position)
+	_weapon.shoot(_world, aim.get_global_position(), aim.get_global_position() - get_global_position())
 
 func _on_death_timer_timeout():
 	dispose()
