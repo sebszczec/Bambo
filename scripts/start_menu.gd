5e8645302a530c0d077a2e899b0f0e0391014567
Sebastian Szczecina <sebszczec@wp.pl>
2025-03-26 20:02:17 +0100

    warp effect is now done with animation player

--- scripts/start_menu.gd
+++ scripts/start_menu.gd
@@ -4,63 +4,22 @@ extends Control
 @onready var settingsButton = $GridContainer/Settings
 @onready var quitButton = $GridContainer/Quit
 @onready var container = $GridContainer
+@onready var anim = $AnimationPlayer
 @onready var warp = $Warp
 
-var start_game_delay_1 : float = 1
-var fade_step = 1 / start_game_delay_1
-var fade : bool = false
-var start_game_timer_1 = Timer.new()
-
-var start_game_delay_2 : float = 1
-var speed_step = 1 / start_game_delay_2
-var brightness_step = 2.5 / start_game_delay_2
-var warp_speed : float = 0
-var warp_brightness : float = 0
-var start_game_timer_2 = Timer.new()
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	start_game_timer_1.one_shot = true
-	start_game_timer_1.wait_time = start_game_delay_1
-	start_game_timer_1.connect("timeout", _on_start_game_timer_1_timeout)
-	add_child(start_game_timer_1)
-	
-	start_game_timer_2.one_shot = true
-	start_game_timer_2.wait_time = start_game_delay_2
-	start_game_timer_2.connect("timeout", _on_start_game_timer_2_timeout)
-	add_child(start_game_timer_2)
-	
-	warp_speed = 3.0
-	warp_brightness = 5.0
-	container.modulate.a = 0.5
-	print (warp_speed)
-	
 	newGameButton.grab_focus()
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	if fade:
-		container.modulate.a = abs(container.modulate.a - fade_step * delta)
-		warp_speed += speed_step * delta
-		warp.set_speed(warp_speed)
-		warp_brightness = abs(warp_brightness - brightness_step * delta)
-		warp.set_brightness(warp_brightness)
-
-
-func _on_start_game_timer_1_timeout() -> void:
-	start_game_timer_2.start()
-
-func _on_start_game_timer_2_timeout() -> void:
-	get_tree().change_scene_to_file("res://scenes/count_down.tscn")
 
 func _on_quit_pressed() -> void:
 	get_tree().quit()
 
 
 func _on_new_game_pressed() -> void:
-	fade = true
-	start_game_timer_1.start()
+	anim.play("warp_speed")
 
 
 func _on_settings_pressed() -> void:
@@ -81,3 +40,9 @@ func _on_settings_mouse_entered() -> void:
 
 func _on_quit_mouse_entered() -> void:
 	quitButton.grab_focus()
+
+
+func _on_animation_player_animation_finished(_anim_name: StringName) -> void:
+	warp.Speed = 3.0
+	warp.Brightness = 5.0
+	get_tree().change_scene_to_file("res://scenes/count_down.tscn")
--- scripts/warp.gd
+++ scripts/warp.gd
@@ -2,14 +2,21 @@ extends Node2D
 
 @onready var shader_material = $ColorRect.material
 
-func set_speed(value : float) -> void:
-	shader_material.set_shader_parameter("speed", value)
+@export var Speed : float = 3.0
+@export var Brightness : float = 5.0
 
-func get_speed() -> float:
-	return shader_material.get_shader_parameter("speed")
-	
-func set_brightness(value : float) -> void:
-	shader_material.set_shader_parameter("brightness", value)
+func _physics_process(_delta: float) -> void:
+	shader_material.set_shader_parameter("speed", Speed)
+	shader_material.set_shader_parameter("brightness", Brightness)
 
-func get_brightness() -> float:
-	return shader_material.get_shader_parameter("brightness")
+#func set_speed(value : float) -> void:
+	#shader_material.set_shader_parameter("speed", value)
+#
+#func get_speed() -> float:
+	#return shader_material.get_shader_parameter("speed")
+	#
+#func set_brightness(value : float) -> void:
+	#shader_material.set_shader_parameter("brightness", value)
+#
+#func get_brightness() -> float:
+	#return shader_material.get_shader_parameter("brightness")
