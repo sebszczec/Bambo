dd04e4b12d3ac27412918b655e7e6ffd0ce84204
Sebastian Szczecina <sebszczec@wp.pl>
2025-07-25 19:17:52 +0200

    work on player damage - make it dynamic

--- singletons/player_status.gd
+++ singletons/player_status.gd
@@ -3,14 +3,19 @@ extends Node
 var MaxLife: float = 100.0
 var MaxAfterburner : float = 100.0
 var MaxShield : float = 100.0
+var BaseDamage : float = 1
 var CriticalHitChance : float = 10.0
 var CriticalHitMultiplier : float = 2.0
 
 var CurrentWeapon = Enums.WEAPONS.SMALL
 
+signal base_damage_changed (value : float)
+signal critical_hit_chance_changed (value : float)
+signal ciritcal_hit_multipayer_changes (value : float)
+
 func get_damage():
 	var damage = Damage.new()
-	damage.Value = DamageValues.get_damage(CurrentWeapon)
+	damage.Value = BaseDamage * DamageValues.get_damage(CurrentWeapon)
 	damage.Critical = false
 	
 	if randf_range(0.0, 100.0) <= CriticalHitChance:
